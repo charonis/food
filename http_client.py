8e75ad91b807e59b42eb913445a0ee157141d56c
dfsfd <dfkjakfja>
2024-06-23 00:27:45 +0300

    new file:   .gitignore 	new file:   http_client.py 	new file:   main.py 	new file:   model.py 	new file:   myFilter.py 	new file:   requirements.txt 	new file:   vercel.json

---
+++ http_client.py
@@ -0,0 +1,27 @@
+from aiohttp import ClientSession
+    
+class GETListsFood:
+    
+    def __init__(self, header: str):
+        self.header = header
+        
+    async def getList(self):
+        async with ClientSession() as session:
+            async with session.get(f"https://www.themealdb.com/api/json/v1/1/filter.php?i={self.header}") as response:
+                result =  await response.json()
+                return result["meals"]
+            
+            
+            
+            
+class GetFood:
+    
+    def __init__(self, header: str):
+        self.header = header
+        
+    async def getFood(self):
+        async with ClientSession() as session:
+           async with session.get(f"https://www.themealdb.com/api/json/v1/1/lookup.php?i={self.header}") as response:
+                result = await response.json()
+                return result["meals"]
+            
---
+++ main.py
@@ -0,0 +1,48 @@
+from  fastapi import FastAPI, Depends
+from http_client import GETListsFood, GetFood
+from model import ListFood, FoodMain
+from myFilter import MyFilter
+import uvicorn
+
+
+app = FastAPI(
+    title= "listFood"
+)
+
+
+
+def params_list(header: str = "chicken_breast"):
+    return {"header": header}
+
+def params_food(header: str = "52772"):
+    return {"header": header}
+
+@app.get("/")
+async def aa():
+    a = str("hello world")
+    return a
+
+
+@app.get("/foodList", response_model=list[ListFood])
+async def get_food_list(params_list: dict = Depends(params_list)):
+    
+    listFood = GETListsFood(**params_list)
+    
+    return await listFood.getList()
+
+
+@app.get("/mainFood",response_model= FoodMain )
+async def get_food_main(params_food: dict = Depends(params_food)):
+    
+    listFood = GetFood(**params_food)
+    
+    result = await listFood.getFood()
+    
+    myFilter = MyFilter(result[0])
+    
+    return myFilter.myFilter()
+
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
